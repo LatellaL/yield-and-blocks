aad46a6cb477f1c2220d5dfd1ce0f78618e9a753
LatellaL <83371530+LatellaL@users.noreply.github.com>
2021-10-03 19:09:53 -0400

    commit tree ed80736e90725f4c7605f92e51f08d67328e6a84

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,25 @@
-def hello_t
-
+def hello_t(array)
+    if block_given?
+        i = 0
+#hello_t calls the block once for each element in the passed-in array
+#hello_t returns the original array
+#hello_t is not hard-coded
+    while i < array.length
+        yield(array[i])
+        i = i + 1
+    end
+    array
+#hello_t fails gracefully when a block is not passed in
+else
+    puts "Hey! No block was given!"
+    end
 end
 
 # call your method here!
-
+ 
+#hello_t
+["Tim", "Tom", "Jim"].each do |name|
+    if name.start_with?("T")
+        puts "Hi, #{name}"
+    end
+  end
